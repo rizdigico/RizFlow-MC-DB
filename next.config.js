f4e0626e147654d308036ce465bfb8deeb2c8fa7
RizFlow <rizdigi.co@gmail.com>
2026-03-27 15:31:43 +0000

    Simplify Vercel config - let Vercel auto-detect Next.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/hyrve/:path*',
-        destination: 'https://api.hyrveai.com/v1/:path*'
-      }
-    ]
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
